fa006a585d7607b7e5f4891de1ea10d627c2db5e
Kskle555 <92490451+Kskle555@users.noreply.github.com>
2022-04-23 23:15:46 +0300

    Shopping Cart Exercise

---
+++ src/components/store/index.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import uiSlice from "./ui-slice";
+import cartslice from "./cart-slice";
+const store = configureStore({
+    reducer: {ui:uiSlice.reducer, cart: cartslice.reducer}
+});
+
+export default store;
